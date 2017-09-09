ea478524fe00b7f1221e5481d5c87f1b8165b922
zack <>
2017-09-08 19:58:45 -0700

    Add attack box test

--- test/attack_box_test.cpp
+++ test/attack_box_test.cpp
@@ -1,23 +1,51 @@
 #include "catch.hpp"
 #include "../attack_box.h"
-#include "../player.h"
 #include "../object.h"
+#include "../alive.h"
 #include <iostream>
 
 TEST_CASE( "attack_box damages object", "[attack_box]" )
 {
-	//player is the only class at the moment that is an alive object
-	player p;
+	class tmp : public object, public alive
+	{
+		bool think() { return false; };
+	};
+	
+	tmp t;
 
-	p.health(5);
+	t.health(5);
 
 	attack_box att_b;
 
 	att_b.damage(3);
 
-	object* obj = dynamic_cast<object*>(&p);
+	object* obj = dynamic_cast<object*>(&t);
 
 	att_b.action( obj );
 
-	REQUIRE( p.health() == 2 );
+	REQUIRE( t.health() == 2 );
+}
+
+TEST_CASE( "attack_box knocks back object", "[attack_box]" )
+{
+	class tmp : public object
+	{
+		bool think() { return false; };
+	};
+
+	tmp t;
+
+	t.h_speed(0);
+	t.v_speed(0);
+
+	attack_box att_b;
+
+	att_b.knockback(10);
+
+	object* obj = dynamic_cast<object*>(&t);
+
+	att_b.action( obj );
+
+	REQUIRE( t.h_speed() != 0 );
+	REQUIRE( t.v_speed() != 0 );
 }
