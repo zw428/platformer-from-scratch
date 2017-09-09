07f2a86eec1feeb34d4e1ba3c9cb0d70d10be2d6
zack <>
2017-09-09 13:34:04 -0700

    Add vel_accel test

---
+++ test/vel_accel_test.cpp
@@ -0,0 +1,42 @@
+#include "catch.hpp"
+#include "../vel_accel.h"
+
+TEST_CASE( "vel_accel setters/getters work", "[vel_accel]" )
+{
+	vel_accel v;
+
+	v.h_speed(5);
+	v.v_speed(6);
+	v.h_accel(7);
+	v.v_accel(8);
+	
+	CHECK( v.h_speed() == 5);
+	CHECK( v.v_speed() == 6);
+	CHECK( v.h_accel() == 7);
+	CHECK( v.v_accel() == 8);
+}
+
+TEST_CASE( "vel_accel limits speed", "[vel_accel]" )
+{
+	SECTION( "positive" )
+	{
+		vel_accel v;
+
+		v.h_speed(25);
+		v.v_speed(25);
+		
+		CHECK( v.h_speed() == 15);
+		CHECK( v.v_speed() == 15);
+	}
+	
+	SECTION( "negative" )
+	{
+		vel_accel v;
+
+		v.h_speed(-25);
+		v.v_speed(-25);
+		
+		CHECK( v.h_speed() == -15);
+		CHECK( v.v_speed() == -15);
+	}
+}
