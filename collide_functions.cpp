7971ee7674af90e14505c4ed1da2dc0c2567b14e
zack <>
2017-09-23 17:02:50 -0700

    Add box_in_box(box,box)

--- collide_functions.cpp
+++ collide_functions.cpp
@@ -1,4 +1,5 @@
 #include "collide_functions.h"
+#include "box.h"
 
 bool box_in_box( unsigned x1, unsigned y1, unsigned w1, unsigned h1, unsigned x2, unsigned y2, unsigned w2, unsigned h2 )
 {
@@ -8,4 +9,14 @@ bool box_in_box( unsigned x1, unsigned y1, unsigned w1, unsigned h1, unsigned x2
 	}
 
 	return ( x1 < x2 + w2 && x1 + w1 > x2 && y1 + h1 > y2 && y1 < y2 + h2 );
+}
+
+bool box_in_box( box* b1, box* b2 )
+{
+	if ( !b1 || !b2 )
+	{
+		return true;
+	}
+
+	return box_in_box( b1->x(), b1->y(), b1->w(), b1->h(), b2->x(), b2->y(), b2->w(), b2->h() );
 }
--- collide_functions.h
+++ collide_functions.h
@@ -1,6 +1,9 @@
 #ifndef COLLIDE_FUNCTIONS_H
 #define COLLIDE_FUNCTIONS_H
 
+class box;
+
 bool box_in_box( unsigned x1, unsigned y1, unsigned w1, unsigned h1, unsigned x2, unsigned y2, unsigned w2, unsigned h2 );
+bool box_in_box( box* b1, box* b2 );
 
 #endif
